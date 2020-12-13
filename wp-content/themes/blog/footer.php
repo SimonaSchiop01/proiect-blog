fe1ef4cda8b1e99bd9139fbb5c2e8fceda8d9d39
Simona <sts_ellena@yahoo.com>
2020-12-13 04:01:04 -1200

    Adaugare functie wp pentru imgine

--- wp-content/themes/blog/footer.php
+++ wp-content/themes/blog/footer.php
@@ -5,9 +5,9 @@
 	</section>
 	<section class="social-links">
 		<p>Follow </p>
-		<a href="#"><img src="/proiect1/img/facebook.png"></a>
-		<a href="#"><img src="/proiect1/img/twitter.png"></a>
-		<a href="#"><img src="/proiect1/img/instagram.png"></a>
+		<a href="#"><img src="<?php echo get_template_directory_uri() ?>/img/facebook.png"></a>
+		<a href="#"><img src="<?php echo get_template_directory_uri() ?>/img/twitter.png"></a>
+		<a href="#"><img src="<?php echo get_template_directory_uri() ?>/img/instagram.png"></a>
 	</section>
 </footer>
 </body>
